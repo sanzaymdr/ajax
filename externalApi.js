3240086e13d41885c9ed174713610240e6a4cc34
sanzaymdr <sanzaymanandhar99@gmail.com>
2020-12-09 21:52:12 +0545

    ajax image fetching

--- externalApi.js
+++ externalApi.js
@@ -20,3 +20,27 @@ document.getElementById('button').addEventListener('click',userData =>{
     }
     xhr.send();
 })
+// print git users with images 
+document.getElementById('buttonOne').addEventListener('click',imageData=>{
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','https://api.github.com/users' , true);
+
+    xhr.onload = () => {
+        if(xhr.status == 200){
+            var data = JSON.parse(xhr.responseText);
+            imageOutput = '';
+            for(var i in data ){      
+                imageOutput += 
+                    '<img src="'+data[i].avatar_url+'" height="70px" width="70px">'+
+                    '<ul>'
+                    '<li>ID:'+data[i].id+'</li>'
+                    '<li>Username:'+data[i].login+'</li>'
+                    '</ul>';
+            }
+            console.log(data)
+            document.getElementById('data').innerHTML = imageOutput;
+        }
+    }
+
+    xhr.send();
+})
